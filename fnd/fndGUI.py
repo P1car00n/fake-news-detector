1082e9982f155c7bb701ee8ea6df81dd1a54ad78
P1car00n <80679567+P1car00n@users.noreply.github.com>
2022-05-15 21:51:57 +0300

    Add basic menu and "about" window

--- fnd/fndGUI.py
+++ fnd/fndGUI.py
@@ -5,8 +5,7 @@ This module contains all the logic needed to build the program's GUI.
 
 from tkinter import *
 from tkinter import ttk
-from tkinter.messagebox import showinfo
-from turtle import color
+from tkinter import messagebox
 
 
 class FakeDetector:
@@ -14,6 +13,22 @@ class FakeDetector:
     def __init__(self, root) -> None:
 
         root.title('Fake News Detector')
+
+        # Menu
+        root.option_add('*tearOff', FALSE)
+        menubar = Menu(root)
+        root['menu'] = menubar
+
+        menu_pref = Menu(menubar)
+        menu_help = Menu(menubar)
+
+        menubar.add_cascade(menu=menu_pref, label='Preferences')
+        menubar.add_cascade(menu=menu_help, label='Help')
+
+        menu_help.add_command(
+            label='About',
+            command=lambda: self.showAbout(root))
+
         mainframe = ttk.Frame(root, padding='3 3 12 12')
         mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
         root.columnconfigure(0, weight=1)
@@ -45,8 +60,44 @@ class FakeDetector:
         root.bind('<Return>', self.analyse)
 
     def analyse(self, *args):
-        showinfo(self.input_text.get())
+        messagebox.showinfo(self.input_text.get())
         self.analysis_result.set('95% True')
+
+    def showAbout(self, root):
+        # Potentially replace with OOP
+        win_about = Toplevel(root)
+        win_about.title('About Fake News Detector')
+        win_about.columnconfigure(0, weight=1)
+        win_about.rowconfigure(0, weight=1)
+        frame_about = ttk.Frame(win_about, padding='3 3 12 12')
+        frame_about.grid(column=0, row=0, sticky=(N, W, E, S))
+        ttk.Label(
+            frame_about,
+            text='Creator: Arthur Zevaloff').grid(
+            column=0,
+            row=0)
+        ttk.Label(
+            frame_about,
+            text='Made with: Python 3.10.4, Tk 8.6').grid(
+            column=0,
+            row=1)
+        ttk.Label(frame_about,
+                  text='Licensed under the Apache-2.0 license').grid(column=0,
+                                                                     row=2)
+        frame_about.columnconfigure(0, weight=1)
+        frame_about.rowconfigure((0, 1, 2, 3), weight=1)
+        ttk.Button(
+            frame_about,
+            text='Close',
+            command=win_about.destroy,
+            state='active').grid(
+            column=0,
+            row=3,
+            sticky=(E))
+        win_about.bind('<Return>', lambda e: win_about.destroy())
+
+        for child in frame_about.winfo_children():
+            child.grid_configure(padx=5, pady=5)
 
 
 if __name__ == '__main__':
@@ -55,4 +106,5 @@ if __name__ == '__main__':
     root.mainloop()
 
 # TODO: make the arrows point exactly at the respective buttons; 95% True
-# appears not directly under the button 'Analyse'.
+# appears not directly under the button 'Analyse'. Replce entry with
+# text area. add resizing support for the main window. Check macos support
