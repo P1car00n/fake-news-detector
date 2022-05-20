925ee86721df608038c188ea0dd5d2c0b2ae61f2
P1car00n <80679567+P1car00n@users.noreply.github.com>
2022-05-20 12:17:37 +0300

    Start reimplementing logic with classes instead of functions

--- fnd/fnd.py
+++ fnd/fnd.py
@@ -7,58 +7,88 @@ from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 
 
+class Detector:
+
+    def __init__(self, data_path, column_title='text') -> None:
+        self.data_frame = pd.read_csv(data_path)
+        self.labels = self.data_frame.label
+        x_train, x_test, self.y_train, self.y_test = train_test_split(
+            self.data_frame[column_title], self.labels)
+        tfidf_vectorizer = TfidfVectorizer(stop_words='english')
+        self.tfidf_train = tfidf_vectorizer.fit_transform(x_train)
+        self.tfidf_test = tfidf_vectorizer.transform(x_test)
+
+    def predict(self, classifier, vectoriser, text):
+        # using the same vectoriser?
+        vec_newtest = vectoriser.transform(list(text))
+        t_pred = classifier.predict(vec_newtest)
+        return t_pred
+
+
+class PAClassifier(Detector):
+
+    def __init__(self, data_path, column_title='text') -> None:
+        Detector.__init__(data_path, column_title)
+        pac = PassiveAggressiveClassifier()  # add max iter a a later point
+        pac.fit(self.tfidf_train, self.y_train)
+        y_pred = pac.predict(self.tfidf_test)
+        score = accuracy_score(self.y_test, y_pred)
+        # print(f'Accuracy: {round(score*100,2)}%')
+        # print(confusion_matrix(y_test, y_pred, labels=['FAKE', 'REAL']))
+
+
 # Donald Trump says UFO is real
 #input_data = [input()]
 #vectorized_input_data = tfidf_vectorizer.transform(input_data)
 #prediction = pac.predict(vectorized_input_data)
 # print(prediction)
-def createModel():
-    # Read the data into a data frame
-    data_frame = pd.read_csv('/home/arthur/diploma/news.csv')
-
-    # Get the labels
-    labels = data_frame.label
-
-    # Split the dataset
-    x_train, x_test, y_train, y_test = train_test_split(
-        data_frame['text'], labels, test_size=0.3, random_state=7)
-
-    # Initialize a TfidfVectorizer
-    tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
-
-    # Fit and transform train set, transform test set
-    tfidf_train = tfidf_vectorizer.fit_transform(x_train)
-    tfidf_test = tfidf_vectorizer.transform(x_test)
-
-    # Initialize a PassiveAggressiveClassifier
-    pac = PassiveAggressiveClassifier(max_iter=70)
-    pac.fit(tfidf_train, y_train)
-
-    # Predict on the test set and calculate accuracy
-    y_pred = pac.predict(tfidf_test)
-    score = accuracy_score(y_test, y_pred)
-    print(f'Accuracy: {round(score*100,2)}%')
-
-    # Build confusion matrix
-    print(confusion_matrix(y_test, y_pred, labels=['FAKE', 'REAL']))
-
-    return (tfidf_vectorizer, pac)
-
-
-def testModel(*newtext):
-    tfidf_vectorizer, pac = createModel()
-    vec_newtest = tfidf_vectorizer.transform(list(newtext))
-    t_pred = pac.predict(vec_newtest)
-    print(t_pred.shape)
-    return t_pred
-
-
-if __name__ == '__main__':
-    user_input = input('"1" to create a model, "2" to test: ')
-    if user_input == '1':
-        createModel()
-    elif user_input == '2':
-        print(testModel(input('Text = ')))
-
-
-# TODO: rewrite with OOP
+# def createModel():
+#    # Read the data into a data frame
+#    data_frame = pd.read_csv('/home/arthur/diploma/news.csv')
+#
+#    # Get the labels
+#    labels = data_frame.label
+#
+#    # Split the dataset
+#    x_train, x_test, y_train, y_test = train_test_split(
+#        data_frame['text'], labels, test_size=0.3, random_state=7)
+#
+#    # Initialize a TfidfVectorizer
+#    tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
+#
+#    # Fit and transform train set, transform test set
+#    tfidf_train = tfidf_vectorizer.fit_transform(x_train)
+#    tfidf_test = tfidf_vectorizer.transform(x_test)
+#
+#    # Initialize a PassiveAggressiveClassifier
+#    pac = PassiveAggressiveClassifier(max_iter=70)
+#    pac.fit(tfidf_train, y_train)
+#
+#    # Predict on the test set and calculate accuracy
+#    y_pred = pac.predict(tfidf_test)
+#    score = accuracy_score(y_test, y_pred)
+#    print(f'Accuracy: {round(score*100,2)}%')
+#
+#    # Build confusion matrix
+#    print(confusion_matrix(y_test, y_pred, labels=['FAKE', 'REAL']))
+#
+#    return (tfidf_vectorizer, pac)
+#
+#
+# def testModel(*newtext):
+#    tfidf_vectorizer, pac = createModel()
+#    vec_newtest = tfidf_vectorizer.transform(list(newtext))
+#    t_pred = pac.predict(vec_newtest)
+#    print(t_pred.shape)
+#    return t_pred
+#
+#
+# if __name__ == '__main__':
+#    user_input = input('"1" to create a model, "2" to test: ')
+#    if user_input == '1':
+#        createModel()
+#    elif user_input == '2':
+#        print(testModel(input('Text = ')))
+#
+#
+# TODO: rewrite with OOP; add naive baes option?
