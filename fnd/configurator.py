b045f22a5b9e3702086012b4dc283ae13e0cdea4
P1car00n <80679567+P1car00n@users.noreply.github.com>
2022-06-10 20:08:01 +0300

    Improve configuration parser

--- fnd/configurator.py
+++ fnd/configurator.py
@@ -1,26 +1,31 @@
 import configparser
 import os
 
+
 class Configurator:
-    def __init__(self, folder=''): #
+    def __init__(self, folder='./settings', file_name='preferences.ini'):
         self.config = configparser.ConfigParser()
-        if not os.path.exists(path_to_check := './settings'): #
-            os.makedirs(path_to_check)
+        if not os.path.exists(folder):
+            os.makedirs(folder)
             self.config.add_section('Preferences')
             self.config['Preferences']['theme'] = 'clam'
-            self.config['Preferences']['mode'] = 'advanced'
+            self.config['Preferences']['mode'] = 'simple'
             self.update_pref()
-        
-        self.config.read('./settings/preferences.ini') #
 
-    def update_pref(self, **kwargs): # can be replaced with read_dict
+        # subject of rewrite to accept one path whole
+        self._file_path = folder + '/' + file_name
+
+        self.config.read(self._file_path)
+
+    def update_pref(self, **kwargs):  # can be replaced with read_dict
         for key, value in kwargs:
             self.config['Preferences'][key] = value
-        with open('settings/preferences.ini', 'w') as configfile: #
+        with open(self._file_path, 'w') as configfile:
             self.config.write(configfile)
-    
+
     def get_prefs(self):
         return dict(self.config.items('Preferences'))
+
 
 if __name__ == '__main__':
     test = Configurator()
@@ -29,4 +34,3 @@ if __name__ == '__main__':
 
 #theme = config['Preferences']['theme']
 #mode = config['Preferences']['mode']
-
